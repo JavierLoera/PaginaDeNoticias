458c1e3ff12e32706e22327eb416d7c270049cb5
Javier Loera <jesus_loera_15@hotmail.com>
2021-09-08 17:13:55 -0500

    corriendo errores

--- js/main2.js
+++ js/main2.js
@@ -1,42 +1,22 @@
-$(document).ready(main)
+$(document).ready(function () {
+	// Mostramos y ocultamos submenus
+	$(".submenu").click(function () {
+		$(this).children(".children").slideToggle();
+	});
 
-var contador = 1;
-
-function main() {
-    $('.menu_bar').click(function() {
-
-        if (contador == 1) {
-            $('nav').animate({
-                left: '0'
-            });
-            contador = 0;
-        } else {
-            contador = 1;
-            $('nav').animate({
-                left: '-100%'
-            });
-        }
-
-    });
-
-};
-    // Mostramos y ocultamos submenus
-    $('.submenu').click(function() {
-        $(this).children('.children').slideToggle();
-    });
-
-
-$(document).ready(function() {
-    $('.ir-arriba').click(function() {
-        $('body,html').animate({
-            scrollTop: '0px'
-        }, 300);
-    });
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 0) {
-            $('.ir-arriba').slideDown(300);
-        } else {
-            $('.ir-arriba').slideUp(300);
-        }
-    });
+	$(".ir-arriba").click(function () {
+		$("body,html").animate(
+			{
+				scrollTop: "0px",
+			},
+			300
+		);
+	});
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 0) {
+			$(".ir-arriba").slideDown(300);
+		} else {
+			$(".ir-arriba").slideUp(300);
+		}
+	});
 });
